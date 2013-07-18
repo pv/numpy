734258279fdc81bc02fc314ee95a7fb87cd775aa
Blake Griffith <blake.a.griffith@gmail.com>
2013-07-17 23:15:36 -0500

    WIP: New function to find override. Based on @njsmith suggestions.

--- numpy/core/src/private/ufunc_override.h
+++ numpy/core/src/private/ufunc_override.h
@@ -3,6 +3,62 @@
 #include <npy_config.h>
 
 
+/* Normalizes the args passed to ufunc. Returns a tuple of the data
+ * args and dict of the kwargs.
+ */
+NPY_NO_EXPORT PyObject *
+PyUFunc_NormalizeArgs(PyObject *ufunc, PyObject *args, PyObject *kwds) {};
+
+NPY_NO_EXPORT PyObject *
+PyUFunc_CheckOverride(PyObject *ufunc, PyObject *ufunc_method,
+                    PyObject *args, PyObject *kwds)
+{
+    int i;
+    int nargs = PyTuple_GET_SIZE(args);
+    /* Marks if a __numpy_ufunc__ was found. */
+    int found_override = 0;
+
+    PyObject *obj;
+    PyObject *inputs[NPY_MAXARGS];
+    PyObject *numpy_ufunc = NULL;
+    PyObject *res;
+    PyObject *numpy_ufunc_args;
+
+    static char *_reduce_type[] = {
+        "__call__", "reduce", "accumulate", "reduceat"};
+
+    for (i = 0; i < nargs; i++) {
+        obj = PyTuple_GET_ITEM(args, i);
+        if (PyArray_CheckExact(obj) || PyArray_IsAnyScalar(obj)) {
+            continue;
+        }
+
+        numpy_ufunc = PyObject_GetAttrString(obj, "__numpy_ufunc__");
+        if (PyCallable_Check(numpy_ufunc)) {
+            numpy_ufunc_args = PyTuple_Pack(4, ufunc, ufunc_method, i, args);
+            res = PyObject_Call(numpy_ufunc, numpy_ufunc_args, kwds);
+            Py_DECREF(numpy_ufunc);
+            numpy_ufunc = NULL;
+            found_override = 1;
+            if (res == Py_NotImplemented) {
+                continue;
+            }
+            else {
+                return res;
+            }
+        }
+    }
+    if (found_override) {
+        PyErr_SetString(PyExc_TypeError, 
+                "Not implemented for this type.");
+        return NULL;
+    }
+    else {
+        return NULL;
+    }
+}
+
+/* Check for __numpy_ufunc__ and call it appropriately. */
 NPY_NO_EXPORT PyObject *
 PyUFunc_GetOverride(PyObject *ufunc, PyObject *args, PyObject *kwds)
 {
@@ -56,7 +112,7 @@ PyUFunc_GetOverride(PyObject *ufunc, PyObject *args, PyObject *kwds)
 }
 
 NPY_NO_EXPORT int *
-PyUFunc_CheckOverride(PyObject *args)
+PyUFunc_HasOverride(PyObject *args)
 {
     int i;
     int nargs = PyTuple_GET_SIZE(args);
