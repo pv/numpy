f78ab6a10457cbba720b1146fbb84fa98558b405
Blake Griffith <blake.a.griffith@gmail.com>
2013-07-20 15:15:43 -0500

    WIP: Change PyUFunc_CheckOverride mechanism.
    Changed as per suggestions by @pv and @njsmith

--- numpy/core/src/private/ufunc_override.h
+++ numpy/core/src/private/ufunc_override.h
@@ -4,7 +4,6 @@
 #include "numpy/arrayobject.h"
 #include "multiarray/common.h"
 
-
 static PyObject *
 PyUFunc_CheckOverride(PyUFuncObject *ufunc, PyObject *ufunc_method,
                       PyObject *args, PyObject *kwds)
@@ -15,7 +14,9 @@ PyUFunc_CheckOverride(PyUFuncObject *ufunc, PyObject *ufunc_method,
     PyObject *obj;
     PyObject *override_args;
 
-    PyObject *override = NULL;
+    PyObject *override_obj = NULL;
+    PyObject *override_meth = NULL;
+    PyObject *override_res = NULL;
     PyObject *res = NULL;
 
     for (i = 0; i < nargs; i++) {
@@ -25,25 +26,40 @@ PyUFunc_CheckOverride(PyUFuncObject *ufunc, PyObject *ufunc_method,
         }
 
         if (PyObject_HasAttrString(obj, "__numpy_ufunc__")) {
-            override = PyObject_GetAttrString(obj, "__numpy_ufunc__");
+            override_meth = PyObject_GetAttrString(obj, "__numpy_ufunc__");
             override_args = Py_BuildValue("OOiO", ufunc, ufunc_method, i, args);
-            res = PyObject_Call(override, override_args, kwds);
-
-            Py_DECREF(override);
+            res = PyObject_Call(override_meth, override_args, kwds);
             Py_DECREF(override_args);
 
-            if (res == Py_NotImplemented) {
-                continue;
-            }
-            else {
-                return res;
+            if (res != Py_NotImplemented) {
+                if (!override_obj) {
+                    override_obj = obj;
+                    override_res = res;
+                    Py_DECREF(obj);
+                }
+
+                else if (PyObject_IsSubclass(obj->ob_type, 
+                                             override_obj->ob_type)) {
+                    override_obj = obj;
+                    override_res = res;
+                    Py_DECREF(obj);
+                }
             }
         }
     }
-    if (res) {
+    /* 
+     * Once override_meth is set it never is NULL again, so we use it to check
+     * if any of the args had __numpy_ufunc__.
+     */
+    if (override_meth && !override_res) {
         PyErr_SetString(PyExc_TypeError, 
                 "Not implemented for this type.");
+        Py_DECREF(override_meth);
         return NULL;
+    }
+    if (override_res) {
+        Py_DECREF(override_meth);
+        return override_res;
     }
     else {
         return NULL;
